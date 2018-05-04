b3d7f631fd305f09d4f8c6cc5f6d6b2a801ce459
rishi <rishi_sharan@gmail.com>
2018-05-04 16:57:16 -0600

    version 3

--- src/main/java/WidgetApp.java
+++ src/main/java/WidgetApp.java
@@ -7,7 +7,7 @@ public class WidgetApp {
     public int[] prisonForPrincesses(int[] prisons, int[] princesses, int entrance)
     {
         int i = 0, j = 0;
-        Boolean flag = true;\
+        Boolean flag = true;
         int prisonsThatAccomodate[] = new int[prisons.length];
         HashMap<Integer,Integer> prisonMap = new HashMap<Integer, Integer>();
         HashMap<Integer,Integer> princessMap = new HashMap<Integer, Integer>();
@@ -24,18 +24,51 @@ public class WidgetApp {
 
         int princessNumber = 1;
         int prisonNumber = 1;
-
+        int tempPrisonNumber= 1;
+        int[] keys = new int[10];
+        int h=0;
+        int prisonlimit = prisons.length;
+        int key  =1;
+        Map<String, Integer> keyAndCount = new HashMap<>();
           while( biscuit != 0 ) {
               while( prisonNumber <= prisons.length ) {
-                  if (princessMap.get(princessNumber) == prisonMap.get(prisonNumber)) {
+                  if (princessMap.get(princessNumber) == prisonMap.get(prisonNumber))
+                  {
+                      while ( key <= prisonlimit ) {
+                          if (prisonMap.get(prisonNumber) == prisonMap.get(tempPrisonNumber)) {
+                              for (int keyValues : prisonMap.keySet()) {
 
-                      checkIfAnyOtherPlaceExists(prisons[], prisonMap.get( prisonNumber ));
-                      for ( int s =0 ; s < prisons.length ; s++){
-                          if( prisons[i] == prisonCapacity){
-                              prisonsThatAccomodate[s] =
+                                      keys[h] = tempPrisonNumber;
+
+                                      h++;
+                                      key++;
+                                  }
+
+                              }
+                              System.out.println("Keys are " + keys[0] + "  " + keys[1]);
                           }
-                      }
+                          tempPrisonNumber++;
 
+
+//                      while ( h < prisonMap.size()){
+//                          if(prisonMap.get(prisonNumber) == prisonMap.get(tempPrisonNumber)){
+//                              keys[h] = tempPrisonNumber;
+//                              tempPrisonNumber++;
+//                              prisonNumber++;
+//                              h++;
+//                          }
+//                          System.out.print("Keys are "+ keys[0] + "  " +keys[1] );
+//                      }
+
+
+//
+//                      checkIfAnyOtherPlaceExists(prisons[], prisonMap.get( prisonNumber ));
+//                      for ( int s =0 ; s < prisons.length ; s++){
+//                          if( prisons[i] == prisonCapacity){
+//                              prisonsThatAccomodate[s] =
+//                          }
+//                      }
+//
 
 
 
@@ -87,31 +120,31 @@ public class WidgetApp {
         Set set1 = princessMap.entrySet();
         Iterator iterator1 = set1.iterator();
         int k = 0;
-        while (iterator1.hasNext()){
-            Map.Entry mentry = (Map.Entry)iterator1.next();
-            System.out.println( "Key is " +mentry.getKey() + " Value is " +mentry.getValue() );
-            princesses[k] = (int) mentry.getValue();
-            k++;
-        }
+//        while (iterator1.hasNext()){
+//            Map.Entry mentry = (Map.Entry)iterator1.next();
+//            System.out.println( "Key is " +mentry.getKey() + " Value is " +mentry.getValue() );
+//            princesses[k] = (int) mentry.getValue();
+//            k++;
+//        }
         return princesses;
 
     }
 
-    public boolean checkIfAnyOtherPlaceExists(int[] prisons, int prisonCapacity){
-
-        int prisonsThatAccomodate[] = new int[prisons.length];
-        for ( int i =0 ; i < prisons.length ; i++){
-            if( prisons[i] == prisonCapacity){
-               prisonsThatAccomodate[i] =
-            }
-        }
-        return true;
-    }
+//    public boolean checkIfAnyOtherPlaceExists(int[] prisons, int prisonCapacity){
+//
+//        int prisonsThatAccomodate[] = new int[prisons.length];
+//        for ( int i =0 ; i < prisons.length ; i++){
+//            if( prisons[i] == prisonCapacity){
+//               prisonsThatAccomodate[i] =
+//            }
+//        }
+//        return true;
+//    }
 
 
     public static void main(String[] args){
-        int prisons [] = {1,3,2,2};
-        int princesses [] = {1,1,3,2,1};
+        int prisons [] = {1,2,3,3,2,1};
+        int princesses [] = {1,1,1,1,1,1};
         int entrance = 2;
         int result[] = new int[princesses.length];
         WidgetApp object = new WidgetApp();
