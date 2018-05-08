229c6b54b6e3f622d7cc9cdc49d2d514b2fbaa89
rishi <rishi_sharan@gmail.com>
2018-05-08 16:30:36 -0600

    Second version 5 test cases pass.

--- src/main/java/WidgetApp.java
+++ src/main/java/WidgetApp.java
@@ -8,8 +8,8 @@ public class WidgetApp {
     int count = prisonNumber + 1;
     HashMap<Integer,Integer> prisonMap = new HashMap<Integer, Integer>();
     HashMap<Integer,Integer> princessMap = new HashMap<Integer, Integer>();
-    Boolean flag = true;
-    int lookForPrisonCapacity = 0, entranceFlag = 0;
+    Boolean flag = false;
+    int entranceFlag = 0;
     public int[] prisonForPrincesses(int[] prisons, int[] princesses, int entrance )
     {
         int i = 0, j = 0;
@@ -31,101 +31,54 @@ public class WidgetApp {
           while( biscuit != 0 ) {
               while( prisonNumber <= prisons.length) {
                   if (princessMap.get( princessNumber ) == prisonMap.get( prisonNumber ) ) {
-                      prisonMap.put(1, 0);
-                      princessMap.put(princessNumber, 1);
+                      prisonMap.put(prisonNumber, -2);
+                      princessMap.put( princessNumber, prisonNumber );
                       princessNumber++;
                       prisonNumber = 1;
-                      if ( princessNumber > princesses.length){
+                      if ( princessNumber > princesses.length ){
                           biscuit = 0;
                           flag = false;
                           break;
                       }
-                  }else if( princessNumber < princessMap.size() && count <= princessMap.size()){  // check if there is any other prison with capacity that can fit this princess
-
-//                        princessNumber = object.putPrinceInsidePrison( count , princessNumber );
-
-                          if( princessMap.get( princessNumber ) == prisonMap.get( count ) ){
-                              princessMap.put(princessNumber, count);
-                              prisonMap.put(count,0);
-                              princessNumber++;
-                              count = 0;
-                          }
-                          count++;
-                          if ( count == prisonMap.size()){
-                              lookForPrisonCapacity = 1;
-                          }
-
                   }
-
-                  else if (  prisonMap.get( prisonNumber ) == entrance ){     //check for prison i.e close to entrance
+                  else if (  prisonMap.get( prisonNumber ) == entrance && princessMap.get( princessNumber ) == entrance ){     //check for prison i.e close to entrance
+                      prisonMap.put( prisonNumber,-2);
                       princessMap.put(princessNumber, prisonNumber);
-                      prisonMap.put( prisonNumber,0);
                       princessNumber++;
                       prisonNumber = 1;
                       entranceFlag = 1;
 
+                  } else if( princessMap.get( princessNumber ) < prisonMap.get(prisonNumber) ){
+                      prisonMap.put( prisonNumber, -2 );
+                      princessMap.put(princessNumber, prisonNumber);
+                      princessNumber++;
+                      prisonNumber = 1;
+                      if ( princessNumber > princesses.length){
+                          biscuit = 0;
+                          flag = false;
+                          break;
+                      }
                   }
-
-//                  else if( princessMap.get( princessNumber ) < prisonMap.get(prisonNumber) && lookForPrisonCapacity == 1 && entranceFlag == 1){
-//                      prisonMap.put( prisonNumber, 0 );
-//                      princessMap.put(princessNumber, prisonNumber);
-//                      princessNumber++;
-//                      prisonNumber = 1;
-//                      if ( princessNumber > princesses.length){
-//                          biscuit = 0;
-//                          flag = false;
-//                          break;
-//                      }
-//                  }
-                  else{
+                  else {
                       prisonNumber++;
-                  }
-
-              }
-              prisonNumber = 1;
-              while( prisonNumber < prisonMap.size()   ) {
-                  if ( princessMap.get(princessNumber) < prisonMap.get(prisonNumber) && lookForPrisonCapacity == 1 && entranceFlag == 1) {
-                      prisonMap.put(prisonNumber, 0);
-                      princessMap.put(princessNumber, prisonNumber);
-                      princessNumber++;
-                      if ( princessNumber > princesses.length){
-                          biscuit = 0;
-                          flag = false;
-                          break;
-                      }
-                      break;
-                  }
-                  prisonNumber++;
-              }
-
-              prisonNumber = 1;
-              while( prisonNumber <= prisonMap.size()   ) {
-                  if ( princessMap.get(princessNumber) == prisonMap.get(prisonNumber) ) {
-                      prisonMap.put(prisonNumber, 0);
-                      princessMap.put(princessNumber, prisonNumber);
-                      princessNumber++;
-                      if ( princessNumber > princesses.length){
-                          biscuit = 0;
-                          flag = false;
-                          break;
+                      if ( prisonNumber > prisonMap.size()){
+                          flag = true;
                       }
                   }
-                  if ( prisonNumber == prisonMap.size() ){
-                        princessMap.put(princessNumber, -1);
-                        princessNumber++;
-                        prisonNumber = 1;
-                        if ( princessNumber > princesses.length ) {
-                               biscuit = 0;
-                               flag = false;
-                               break;
-                        }
-                  }
-                  prisonNumber++;
               }
+              if( flag.equals(true)) {
 
+                     princessMap.put(princessNumber, -1);
+                      princessNumber++;
+                      prisonNumber = 1;
+                     if (princessNumber > princesses.length) {
+                        biscuit = 0;
+                        flag = false;
+                        break;
+                     }
+              }
 
           }
-
         Set set1 = princessMap.entrySet();
         Iterator iterator1 = set1.iterator();
         int k = 0;
@@ -143,7 +96,7 @@ public class WidgetApp {
     public static void main(String[] args){
         int prisons [] = {1, 3, 2, 2};
         int princesses [] = {1, 1, 3, 2, 1};
-        int entrance = 3;
+        int entrance = 2;
         int result[] = new int[ princesses.length ];
         WidgetApp object = new WidgetApp();
         result = object.prisonForPrincesses(prisons, princesses, entrance);
@@ -167,4 +120,65 @@ public class WidgetApp {
 //        }
 //        return princessNumber;
 //    }
+//                    else if( princessNumber < princessMap.size() && count <= princessMap.size()){  // check if there is any other prison with capacity that can fit this princess
+//
+//                        princessNumber = object.putPrinceInsidePrison( count , princessNumber );
+//
+//                          if( princessMap.get( princessNumber ) == prisonMap.get( count ) ){
+//                              princessMap.put(princessNumber, count);
+//                              prisonMap.put(count,-2);
+//                              princessNumber++;
+//                              count = 0;
+//                          }
+//                          count++;
+//                          if ( count == prisonMap.size()){
+//                              lookForPrisonCapacity = 1;
+//                          }
+//
+//                  }
+
+
+
+
+
+//              }
+//              prisonNumber = 1;
+//              while( prisonNumber < prisonMap.size()   ) {
+//                  if ( princessMap.get(princessNumber) < prisonMap.get(prisonNumber) && lookForPrisonCapacity == 1 && entranceFlag == 1) {
+//                      prisonMap.put(prisonNumber, -2);
+//                      princessMap.put(princessNumber, prisonNumber);
+//                      princessNumber++;
+//                      if ( princessNumber > princesses.length){
+//                          biscuit = 0;
+//                          flag = false;
+//                          break;
+//                      }
+//                      break;
+//                  }
+//                  prisonNumber++;
+//              }
+
+//              prisonNumber = 1;
+//              while( prisonNumber <= prisonMap.size()   ) {
+//                  if ( princessMap.get(princessNumber) == prisonMap.get(prisonNumber) ) {
+//                      prisonMap.put(prisonNumber, -2);
+//                      princessMap.put(princessNumber, prisonNumber);
+//                      princessNumber++;
+//                      if ( princessNumber > princesses.length){
+//                          biscuit = 0;
+//                          flag = false;
+//                          break;
+//                      }
+//                  }
+//                  if ( prisonNumber == prisonMap.size() ){
+//                        princessMap.put(princessNumber, -1);
+//                        princessNumber++;
+//                        prisonNumber = 1;
+//                        if ( princessNumber > princesses.length ) {
+//                               biscuit = 0;
+//                               flag = false;
+//                               break;
+//                        }
+//                  }
+//                  prisonNumber++;
 }
